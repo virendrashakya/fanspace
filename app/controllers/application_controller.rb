03775334f14c5a001121234567ceb4a9ce6f5505
Virendra Shakya <virendrashakya.knp@gmail.com>
2022-11-09 18:28:25 +0530

    devise session working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    home_index_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+  get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 end
